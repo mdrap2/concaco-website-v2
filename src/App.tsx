48a7c826646f360641a2eba4ea58b38a126e1e61
mdrap <13967329+mdrap@users.noreply.github.com>
2024-12-18 11:49:02 -0500

    Revert "Updated To New Which Includes the Privacy Policy.  Side effect is it wipes out my changes"
    This reverts commit 28c73098e63fab39a3d35bc328bfbf22220d41e5.

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
-import PrivacyPolicy from "./pages/PrivacyPolicy";
 
 const queryClient = new QueryClient();
 
@@ -16,7 +15,6 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
