12f6446f291c7db0abcd04892bc8e4441baeeb79
mdrap <13967329+mdrap@users.noreply.github.com>
2024-12-18 12:37:31 -0500

    Trying to add privacy policy

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,10 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Index from "./pages/Index";
+import PrivacyPolicy from "./pages/PrivacyPolicy";
+import 
 
 const queryClient = new QueryClient();
 
@@ -15,6 +18,7 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
+          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
