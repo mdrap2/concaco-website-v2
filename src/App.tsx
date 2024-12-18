c81dc7e1b6708ef045b0c1eb8755ec9cbce905b7
mdrap <13967329+mdrap@users.noreply.github.com>
2024-12-18 12:39:08 -0500

    Revert "Trying to add privacy policy"
    This reverts commit 12f6446f291c7db0abcd04892bc8e4441baeeb79.

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import Index from "./pages/Index";
-import PrivacyPolicy from "./pages/PrivacyPolicy";
-import 
 
 const queryClient = new QueryClient();
 
@@ -18,7 +15,6 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
